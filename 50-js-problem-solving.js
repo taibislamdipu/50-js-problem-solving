0969255f1ec8392eaa971b6d6c95ccb100c8a9bc
taibislamdipu <mailtaibislam@gmail.com>
2023-03-28 03:52:39 +0600

    17 done

--- 50-js-problem-solving.js
+++ 50-js-problem-solving.js
@@ -133,3 +133,15 @@ function isPrime(n) {
 }
 
 console.log(isPrime(11));
+
+// #17: Calculate the sum of digits of a positive integer number
+let intDigits = [];
+
+function sumPositiveIntegerDigits(n) {
+  const digits = n.toString().split("");
+  digits.map((digit) => intDigits.push(parseInt(digit)));
+  const sum = intDigits.reduce((cur, digit) => cur + digit);
+  return sum;
+}
+
+console.log(sumPositiveIntegerDigits(10134));
