bbae7f4e06f2505c769c02cd248c85eb37c0c4a4
taibislamdipu <mailtaibislam@gmail.com>
2023-04-14 02:45:53 +0600

    new

---
+++ mergeArray/mergeUniqueArray.ts
@@ -0,0 +1,29 @@
+// #25: Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both
+
+// solution 1
+function mergeUniqueArray(arr1: number[], arr2: number[]) {
+  let unique: number[] = [];
+  const mergedArray = arr1.concat(arr2);
+
+  for (let i = 0; i < mergedArray.length; i++) {
+    if (unique.includes(mergedArray[i])) {
+      continue;
+    } else {
+      unique.push(mergedArray[i]);
+    }
+  }
+  return unique;
+}
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 3, 20, 5, 10];
+
+console.log(mergeUniqueArray(arr1, arr2));
+
+// solution 2
+function mergeUniqueArray2(arr1: number[], arr2: number[]) {
+  const unique = new Set([...arr1, ...arr2]);
+  return Array.from(unique);
+}
+
+console.log(mergeUniqueArray2(arr1, arr2));
